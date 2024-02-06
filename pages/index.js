db3f4629285f2174e297f974ef3b25c05564e600
MarFelix <felix.mar1995@hotmail.com>
2024-02-06 13:45:27 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title> Bienvenido !</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Bienvenido Carlos Jeremias !" />
+        <Header title="Bienvenido Karla Isabel !" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
